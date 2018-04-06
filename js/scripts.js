cf60a650c9c4cb152435b2b544b599db0613487a
verna <vernajs@gmail.com>
2018-04-06 12:16:03 -0700

    Added logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,19 @@ function Contact(first, last, date) {
   this.accounts = [];
 }
 
-function BankAccount(initial, deposit, withdrawal) {
+function BankAccount(initial, deposit, withdrawal, size, toppings) {
   this.initial = 12.00;
   this.deposit = 5.00;
   this.withdrawal = 2.00;
+  this.size = "test";
+  this.toppings = 1.00;
   //this.balance = balance;
 }
-
 Contact.prototype.fullName = function() {
   return this.firstName + "-" + this.lastName + " " + this.date;
 }
 BankAccount.prototype.fullAccount = function() {
-  return this.initial + this.deposit - this.withdrawal;
+  return this.initial + " " + this.deposit + " " + this.withdrawal + " " + this.size + "10.00 " + this.toppings;
 }
 var subtract = function(inputtedDeposit, inputtedWithdrawal){
 	return inputtedDeposit - inputtedWithdrawal;
@@ -47,9 +48,13 @@ event.preventDefault();
       var inputtedInitial = parseInt ($(this).find("input.new-initial").val());
       var inputtedDeposit = parseInt ($(this).find("input.new-deposit").val());
       var inputtedWithdrawal = parseInt ($(this).find("input.new-withdrawal").val());
+      var selectSize = parseInt ($(this).find("select.new-size").val());
+      var selectToppings = parseInt ($(this).find("select.new-toppings").val());
+
       //var inputtedBalance = $(this).find("input.new-balance").val();
-      var newAccount = new BankAccount(inputtedInitial, inputtedDeposit, inputtedWithdrawal);
+      var newAccount = new BankAccount(inputtedInitial, inputtedDeposit, inputtedWithdrawal, selectSize, selectToppings);
       newContact.accounts.push(newAccount)
+      alert(BankAccount);
 
       var result = subtract (inputtedDeposit, inputtedWithdrawal);
       $("#Balance").text(result);
@@ -59,14 +64,17 @@ event.preventDefault();
 
 
 
+
     $(".contact").last().click(function() {
       $("#show-contact").show();
       //$("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
       $("ul#accounts").text("");
+
       newContact.accounts.forEach(function(BankAccount) {
         $("ul#accounts").append("<li>" + BankAccount.fullAccount() +"</li>");
+
 
 
       });
